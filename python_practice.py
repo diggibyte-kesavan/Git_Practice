88c85826a30c75d7f0a30c93eb4033564b0f1cc1
kesavan31072001 <kesavan31072001@gmail.com>
2024-03-07 11:30:00 +0530

    added pascal case variable name

--- python_practice.py
+++ python_practice.py
@@ -1,14 +1,14 @@
 # python variables
 
-a=5
-b="kesavan"
+a = 5
+b = "kesavan"
 
 print(a)
 print(b)
 
 # variables case-sensitive
 
-b=10
-B=12
+b = 10
+MyVariableNamae = "Diggibyte"  # pascal case
 print(b)
-print(B)
+print(MyVariableNamae)
