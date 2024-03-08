a0e85ac4ea20a05a59c0335701a77417a58f2281
kesavan31072001 <kesavan31072001@gmail.com>
2024-03-08 19:00:21 +0530

    added the python strings file and write some codes

---
+++ python_Strings.py
@@ -0,0 +1,17 @@
+name_string = "kesava"
+print(name_string)
+
+set_word = "kesk"
+
+if len(set_word) == len(set(set_word)):
+    print("it is a unique word")
+else:
+    print("not a unique word")
+
+
+set_word = "kes"
+
+if len(set_word)==len(set(set_word)):
+    print("it is a unique word")
+else:
+    print("not a unique word")
